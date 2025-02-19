96d9e5d4e2109a64c98a5c6fc9e0ac02214a70e3
BruNah514 <mikemulu713@yahoo.com>
2025-02-18 19:57:53 -0500

    Amazon-clone-2025

---
+++ src/Components/Carousel/CarouselEffect.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import { img } from "./img/data";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import styles from "./carousel.module.css"
+const CarouselEffect = () => {
+  return (
+    <div>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showThumbs={false}
+      >
+        {img.map((ImageItemlink,index) => {
+          return <img key={index} src={ImageItemlink} />;
+        })}
+      </Carousel>
+      <div className={styles.carouselImage}>
+        
+      </div>
+    </div>
+  );
+};
+export default CarouselEffect;
